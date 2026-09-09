c45afc0c49d5c0fcaef10ea93f677373255013be
Juan Hoyos <juanandreshoyosocampo@gmail.com>
2026-09-09 18:09:13 -0500

    ...

--- frontend/js/cliente.js
+++ frontend/js/cliente.js
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = "https://mvp-mercados-viva-backend.onrender.com/";
 
 const token = localStorage.getItem("access_token");
 const usuarioGuardado = localStorage.getItem("usuario");
