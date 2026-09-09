f86183e46c6eb58fc2379a937f6192d84059a98a
Juan Hoyos <juanandreshoyosocampo@gmail.com>
2026-09-09 18:31:49 -0500

    ...

--- backend/main.py
+++ backend/main.py
@@ -24,7 +24,7 @@ security = HTTPBearer()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5500",'https://mvp-mercados-viva.vercel.app/'],
+    allow_origins=["http://127.0.0.1:5500", "https://mvp-mercados-viva.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
